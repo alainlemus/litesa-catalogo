df02c221f8db8db95af494a7993276d733c1beb8
alainlemus <alainttlm@gmail.com>
2025-06-27 13:56:38 -0600

    feat: agrega SEO en pagina de politicas de privacidad

--- resources/views/livewire/privacy-policy-page.blade.php
+++ resources/views/livewire/privacy-policy-page.blade.php
@@ -1,5 +1,17 @@
 <div class="w-full bg-white dark:bg-gray-900">
 
+    @php
+        $siteSettings = \App\Models\SiteSetting::first();
+        $imageLogo =$siteSettings->logo_light;
+    @endphp
+
+    @section('title', 'Aviso de Privacidad - Grupo Litesa')
+    @section('meta_description', 'Lee nuestro aviso de privacidad y conoce cómo protegemos tu información.')
+    @section('og_title', 'Aviso de Privacidad - Grupo Litesa')
+    @section('og_description', 'Lee nuestro aviso de privacidad y conoce cómo protegemos tu información.')
+    @section('og_image',  App::environment('local') ? asset('site/' . ltrim($imageLogo, '/')) : Storage::disk('s3')->url($imageLogo))
+
+
     <div class="container max-w-4xl px-4 py-8 mx-auto">
         <h1 class="mb-6 text-3xl font-bold text-gray-800 dark:text-white">Aviso de Privacidad</h1>
 

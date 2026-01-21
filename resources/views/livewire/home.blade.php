ad6d51a940d7c1f1039ea57180292e408d72c62f
alainlemus <alainttlm@gmail.com>
2026-01-21 16:10:52 -0600

    fix: corrige links de servicios

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -129,10 +129,12 @@
                             {{ $service['description'] }}
                         </p>
 
-                        <a href="{{ $service['url'] ? route($service['url']) : route('home') }}" class="flex items-center -mx-1 text-sm text-blue-500 capitalize transition-colors duration-300 transform dark:text-blue-400 hover:underline hover:text-blue-600 dark:hover:text-blue-500">
-                            <span class="mx-1">ver más</span>
-                            <svg class="w-4 h-4 mx-1 rtl:-scale-x-100" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                        </a>
+                        @if ($service['url'])
+                            <a href="{{ $service['url'] ? route($service['url']) : route('home') }}" class="flex items-center -mx-1 text-sm text-blue-500 capitalize transition-colors duration-300 transform dark:text-blue-400 hover:underline hover:text-blue-600 dark:hover:text-blue-500">
+                                <span class="mx-1">ver más</span>
+                                <svg class="w-4 h-4 mx-1 rtl:-scale-x-100" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                            </a>
+                        @endif
                     </div>
                 @endforeach
             </div>
